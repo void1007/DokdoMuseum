ac09897c02997aa10505c624b94651269fc1aa10
void1007 <145522686+void1007@users.noreply.github.com>
2024-05-24 17:14:34 +0900

    2024-05-24

---
+++ 05-24/javascript.js
@@ -0,0 +1,15 @@
+// const container = document.querySelector(".container");
+// console.log(container);
+// const divArray = document.querySelectorAll("div");
+// divArray.forEach((div)=>console.log(div));
+const cardArray = document.querySelectorAll(".namecard");
+
+// cardArray[0].addEventListener("click",()=>{
+//     console.log(cardArray[0]);
+// })
+
+// cardArray.forEach((card)=>{
+//     card.addEventListener("click",()=>{
+//         console.log(card);
+//     })
+// })
